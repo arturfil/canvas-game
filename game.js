dc093e1ce6425660028412494bcc3086f9fbbe63
arturfil <arturfil@hotmail.com>
2021-10-28 15:17:35 -0400

    added image of labrador

--- game.js
+++ game.js
@@ -3,8 +3,10 @@ canvas.height = 550;
 canvas.width = 600;
 canvas.style.borderColor = 'red';
 
+// creating context as 'ctx'
 const ctx = canvas.getContext('2d');
 
+// ctx.fillStyle = 'blue';
 ctx.fillRect(30, 30, 200, 200);
 ctx.clearRect(30, 30, 200, 200)
 
@@ -22,9 +24,36 @@ ctx.stroke();
 ctx.closePath();
 
 ctx.beginPath();
-ctx.arc(150, 150, 40, 0, Math.PI * 3/2);
+ctx.lineWidth = 8;
+ctx.arc(150, 150, 40, 0, Math.PI * 2);
+ctx.fillStyle = 'green'
+ctx.strokeStyle = 'red';
 ctx.stroke(); // draw
-// ctx.fill();
+ctx.fill();
+
+// creating text in canvas
+ctx.strokeStyle = 'blue';
+ctx.lineWidth = 1;
+ctx.font = '50px Calibri';
+ctx.strokeText("Hello World", 350, 300);
+
+// player example
+// ctx.fillStyle = 'yellow'
+// creating square as player
+ctx.fillRect(400, 400, 40, 40);
+
+// display image of labrador
+const img = new Image();
+img.src = 'https://static.pexels.com/photos/8700/wall-animal-dog-pet.jpg';
+
+let imgScale = 640/650;
+
+img.onload = () => {
+  ctx.drawImage(img, 150, 150, 150 * 0.7, 100);
+  ctx.drawImage()
+}
+
+
 
 // class Game {
 //   constructor(rows, columns) {
